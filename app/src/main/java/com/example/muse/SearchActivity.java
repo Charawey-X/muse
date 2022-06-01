2b0580a1d0e66abb09d9605c91f7fb7d18794ef3
Charawey-X <charawey@gmail.com>
2022-06-01 08:15:27 +0300

    edit images

--- app/src/main/java/com/example/muse/SearchActivity.java
+++ app/src/main/java/com/example/muse/SearchActivity.java
@@ -2,13 +2,29 @@ package com.example.muse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity {
+    
+    @BindView(R.id.searchGrid) GridView searchGrid;
+    
+    String[] searchItems = new String[] {"Search Artist","Search Lyrics","Search Song"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+
     }
+
+    
 }
