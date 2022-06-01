c37ffc3893d9e0d1391f9d800e961e70a2fe79cf
Charawey-X <charawey@gmail.com>
2022-06-01 08:38:50 +0300

    add input

--- app/src/main/java/com/example/muse/SearchActivity.java
+++ app/src/main/java/com/example/muse/SearchActivity.java
@@ -2,27 +2,17 @@ package com.example.muse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Toast;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity {
     
-    @BindView(R.id.searchGrid) GridView searchGrid;
-    
-    String[] searchItems = new String[] {"Search Artist","Search Lyrics","Search Song"};
+    String[] searchItems = new String[] {"Your Song Title Will","Smells Like Teen Spirit", "Billie Jean", "Stayin' Alive", "I Will Survive", "Whole Lotta Love", "Sweet Child O'Mine"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        ButterKnife.bind(this);
 
     }
 
