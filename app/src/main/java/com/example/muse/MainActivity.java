3c5cadc83fe84ded5bd03c648263b98c5a7fdc55
Charawey-X <charawey@gmail.com>
2022-06-01 06:24:48 +0300

    add toast and intent

--- app/src/main/java/com/example/muse/MainActivity.java
+++ app/src/main/java/com/example/muse/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.muse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("NonConstantResourceId")
     @BindView(R.id.button_home) Button mHome;
 
     @Override
@@ -17,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mHome.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this,R.string.toast_home_button,Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this,SearchActivity.class);
+            startActivity(intent);
+        });
     }
 }
