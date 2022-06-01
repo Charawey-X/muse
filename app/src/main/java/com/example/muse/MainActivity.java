61638ccaa643e367d283e72b1e30972aa1b26234
Charawey-X <charawey@gmail.com>
2022-06-01 06:11:19 +0300

    add search activity

--- app/src/main/java/com/example/muse/MainActivity.java
+++ app/src/main/java/com/example/muse/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.muse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_home) Button mHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
