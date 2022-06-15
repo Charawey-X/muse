2370bfd5e2f035d76d330f9efeafea8aaa2a5cd9
Charawey-X <charawey@gmail.com>
2022-06-15 21:50:42 +0300

    add API to build and constants

---
+++ app/src/main/java/com/example/muse/Constants.java
@@ -0,0 +1,6 @@
+package com.example.muse;
+
+public class Constants {
+    public static final String YELP_BASE_URL = "https://genius.p.rapidapi.com/search";
+    public static final String YELP_API_KEY = BuildConfig.GENIUS_API_KEY;
+}
