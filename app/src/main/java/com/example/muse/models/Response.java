c9adec7c6885d49fd840b6dbfbd805dca26ccb05
Charawey-X <charawey@gmail.com>
2022-06-22 13:57:21 +0300

    edit response

---
+++ app/src/main/java/com/example/muse/models/Response.java
@@ -0,0 +1,40 @@
+
+package com.example.muse.models;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Response {
+
+    @SerializedName("hits")
+    @Expose
+    private List<Hit> hits = null;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Response() {
+    }
+
+    /**
+     * 
+     * @param hits
+     */
+    public Response(List<Hit> hits) {
+        super();
+        this.hits = hits;
+    }
+
+    public List<Hit> getHits() {
+        return hits;
+    }
+
+    public void setHits(List<Hit> hits) {
+        this.hits = hits;
+    }
+
+}
