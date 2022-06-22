62e5e743fdfc5811f31c2c3d6783d0d5183fa981
Charawey-X <charawey@gmail.com>
2022-06-22 13:57:47 +0300

    edit releasedate

---
+++ app/src/main/java/com/example/muse/models/ReleaseDateComponents.java
@@ -0,0 +1,65 @@
+
+package com.example.muse.models;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class ReleaseDateComponents {
+
+    @SerializedName("year")
+    @Expose
+    private Integer year;
+    @SerializedName("month")
+    @Expose
+    private Integer month;
+    @SerializedName("day")
+    @Expose
+    private Integer day;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public ReleaseDateComponents() {
+    }
+
+    /**
+     * 
+     * @param month
+     * @param year
+     * @param day
+     */
+    public ReleaseDateComponents(Integer year, Integer month, Integer day) {
+        super();
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public Integer getDay() {
+        return day;
+    }
+
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+
+}
