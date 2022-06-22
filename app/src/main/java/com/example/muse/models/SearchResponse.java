57deeb13da864972cc29666c93b313325ae1330a
Charawey-X <charawey@gmail.com>
2022-06-22 13:56:27 +0300

    edit stats

---
+++ app/src/main/java/com/example/muse/models/SearchResponse.java
@@ -0,0 +1,52 @@
+
+package com.example.muse.models;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class SearchResponse {
+
+    @SerializedName("meta")
+    @Expose
+    private Meta meta;
+    @SerializedName("response")
+    @Expose
+    private Response response;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public SearchResponse() {
+    }
+
+    /**
+     * 
+     * @param meta
+     * @param response
+     */
+    public SearchResponse(Meta meta, Response response) {
+        super();
+        this.meta = meta;
+        this.response = response;
+    }
+
+    public Meta getMeta() {
+        return meta;
+    }
+
+    public void setMeta(Meta meta) {
+        this.meta = meta;
+    }
+
+    public Response getResponse() {
+        return response;
+    }
+
+    public void setResponse(Response response) {
+        this.response = response;
+    }
+
+}
